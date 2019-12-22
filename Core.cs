266d6cb37b0616ab47a12a94a510b817f66736a7
nvdone <github@nvd.one>
2019-12-22 17:07:16 +0300

    Initial commit, actually.

---
+++ Core.cs
@@ -0,0 +1,93 @@
+﻿//NVD Core Excel functions and commands
+//Copyright © 2019, Nikolay Dudkin
+
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//GNU General Public License for more details.
+//You should have received a copy of the GNU General Public License
+//along with this program.If not, see<https://www.gnu.org/licenses/>.
+
+using ExcelDna.Integration;
+using Microsoft.Office.Interop.Excel;
+using System;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace Core
+{
+    public static class Core
+    {
+        [ExcelFunction(Description = "RegExIsMatch")]
+        public static bool RegExIsMatch(string input, string pattern)
+        {
+            Regex regex = new Regex(pattern);
+            return (regex.IsMatch(input));
+        }
+
+        [ExcelFunction(Description = "RegExMatch (string input, string pattern, int group = 0, int capture = 0)")]
+        public static string RegExMatch(string input, string pattern, int group = 0, int capture = 0)
+        {
+            Regex regex = new Regex(pattern);
+            Match match = regex.Match(input);
+            if (!match.Success)
+                return "";
+            return match.Groups[group]?.Captures[capture]?.Value ?? "";
+        }
+
+        [ExcelFunction(Description = "RegExMatches (string input, string pattern, int match = 0, int group = 0, int capture = 0)")]
+        public static string RegExMatches(string input, string pattern, int match = 0, int group = 0, int capture = 0)
+        {
+            Regex regex = new Regex(pattern);
+            if (!regex.IsMatch(input))
+                return "";
+
+            MatchCollection matches = regex.Matches(input);
+
+            return matches[match]?.Groups[group]?.Captures[capture]?.Value ?? "";
+        }
+
+        [ExcelFunction(Description = "RegExReplace( string input, string pattern, string replacement)")]
+        public static string RegExReplace(string input, string pattern, string replacement)
+        {
+            Regex regex = new Regex(pattern);
+            return regex.Replace(input, replacement);
+        }
+
+        [ExcelFunction(Description = "CoreAbout()")]
+        public static string CoreAbout()
+        {
+            Assembly executingAssembly = Assembly.GetExecutingAssembly();
+            return ((AssemblyDescriptionAttribute)Attribute.GetCustomAttribute(executingAssembly, typeof(AssemblyDescriptionAttribute))).Description + " " + executingAssembly.GetName().Version.ToString() + ", " + ((AssemblyCopyrightAttribute)Attribute.GetCustomAttribute(executingAssembly, typeof(AssemblyCopyrightAttribute))).Copyright;
+        }
+
+        [ExcelCommand(Description = "Switch reference style between R1C1 and A1.")]
+        public static void SwitchReferenceStyle()
+        {
+            Application excel = (Application)ExcelDnaUtil.Application;
+            if (excel.Application.ReferenceStyle == XlReferenceStyle.xlA1)
+                excel.Application.ReferenceStyle = XlReferenceStyle.xlR1C1;
+            else
+                excel.Application.ReferenceStyle = XlReferenceStyle.xlA1;
+        }
+
+        [ExcelCommand(Description = "Switch precision as displayed.")]
+        public static void SwitchPrecisionAsDisplayed()
+        {
+            Application excel = (Application)ExcelDnaUtil.Application;
+            if(excel.Application.ActiveWorkbook != null)
+                excel.Application.ActiveWorkbook.PrecisionAsDisplayed = !excel.Application.ActiveWorkbook.PrecisionAsDisplayed;
+        }
+
+        [ExcelCommand(Description = "Switch allow editing directly in cells.")]
+        public static void SwitchAllowEditingDirectlyInCells()
+        {
+            Application excel = (Application)ExcelDnaUtil.Application;
+            excel.Application.EditDirectlyInCell = !excel.Application.EditDirectlyInCell;
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Core")]
+[assembly: AssemblyDescription("NVD Core Excel functions and commands")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Nikolay Dudkin")]
+[assembly: AssemblyProduct("NVD Core Excel functions and commands")]
+[assembly: AssemblyCopyright("Copyright © 2019, Nikolay Dudkin")]
+[assembly: AssemblyTrademark("Copyright © 2019, Nikolay Dudkin")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d1982371-680d-44e1-a2fd-246e7ca90e4b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+//[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
